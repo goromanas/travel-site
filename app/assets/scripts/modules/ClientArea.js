29e5e347e1b8b6b67e150075a3ed4a22b9a3bd3a
Romanas Golovko <ro@Romanass-MacBook-Pro.local>
2020-04-22 20:41:13 +0300

    Corrected the invalid password rule

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -21,7 +21,7 @@ class ClientArea {
             this.form.remove();
             this.contentArea.innerHTML = response.data;
         }).catch(() => {
-            this.contentArea.innerHTML = `<p class="client-area__error>That secret phrase is not correct. Try again!</p>`;
+            this.contentArea.innerHTML = `<p class="client-area__error">That secret phrase is not correct. Try again!</p>`;
             this.field.value = '';
             this.field.focus();
         });
