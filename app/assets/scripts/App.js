f7158e83f24331b09e9adffe5eb2b176658f5a43
Romanas Golovko <ro@Romanass-MacBook-Pro.local>
2020-04-22 16:01:21 +0300

    Completed lazy loading of images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+
 
 new StickyHeader();
 
