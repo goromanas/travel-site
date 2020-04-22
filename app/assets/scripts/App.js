3fd2c018976bd683aa7225f3ee7017fa239a6297
Romanas Golovko <ro@Romanass-MacBook-Pro.local>
2020-04-22 20:39:07 +0300

    Added client area feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,10 @@ import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import ClientArea from './modules/ClientArea';
 
 new StickyHeader();
+new ClientArea();
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
